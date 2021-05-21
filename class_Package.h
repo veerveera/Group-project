8c34af3c04eac05c4dfd76ce18f114ba037756f7
swagdudeji <78916480+swagdudeji@users.noreply.github.com>
2021-05-21 14:04:09 +0300

    Add files via upload

---
+++ class_Package.h
@@ -0,0 +1,26 @@
+#ifndef CLASS_PACKAGE_H_INCLUDED
+#define CLASS_PACKAGE_H_INCLUDED
+
+#include <cmath>
+
+class Package{
+
+  private:
+    int currentCoord;
+    int finishCoord;
+    int direction;
+
+  public:
+
+    Package(int tmp_currentCoord, int tmp_finishCoord){
+      currentCoord = tmp_currentCoord
+      finishCoord = tmp_finishCoord
+      direction = (finishCoord-currentCoord)/abs((finishCoord-currentCoord)
+      };
+    
+    void setCurrentCoord(int coord){
+      currentCoord = coord
+    };
+  }
+
+#endif
