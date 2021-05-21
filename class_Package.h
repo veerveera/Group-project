a7f11f9dec285e58b4011b7a4718638947178b90
swagdudeji <78916480+swagdudeji@users.noreply.github.com>
2021-05-21 17:37:40 +0300

    Add files via upload

--- class_Package.h
+++ class_Package.h
@@ -1,7 +1,7 @@
-#ifndef CLASS_PACKAGE_H_INCLUDED
-#define CLASS_PACKAGE_H_INCLUDED
-
 #include <cmath>
+#include <string>
+
+using namespace std;
 
 class Package{
 
@@ -9,22 +9,26 @@ class Package{
     int currentCoord;
     int finishCoord;
     int direction;
+    string recipientName;
+    string senderName;
 
   public:
 
-    Package(int tmp_currentCoord, int tmp_finishCoord){
-      currentCoord = tmp_currentCoord
-      finishCoord = tmp_finishCoord
-      direction = (finishCoord-currentCoord)/abs((finishCoord-currentCoord)
+    Package(int tmp_currentCoord, int tmp_finishCoord, string tmp_recipientName, string tmp_senderName){
+      currentCoord = tmp_currentCoord;
+      finishCoord = tmp_finishCoord;
+      direction = (finishCoord-currentCoord)/abs((finishCoord-currentCoord));
+      recipientName = tmp_recipientName;
+      senderName = tmp_senderName;
       };
     
     void setCurrentCoord(int coord){
-      currentCoord = coord
+      currentCoord = coord;
     };
 
-    void getCurrentCoord(){return currentCoord};
-    void getFinishCoord(){return finishCoord};
-    void getDirection(){return direction};
-  }
-
-#endif
+    int getCurrentCoord(){return currentCoord;};
+    int getFinishCoord(){return finishCoord;};
+    int getDirection(){return direction;};
+    string getRecipientName(){return recipientName;};
+    string getSenderName(){return senderName;};
+  };
